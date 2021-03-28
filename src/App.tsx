3117257ebd1e34f83cb2530cb97ad954e18e3bbe
Lorenzo Imperatrice <lorenzo.imperatrice@ovalmoney.com>
2021-03-28 21:56:51 +0200

    Add store and ioc context to root component

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {container, ContainerContext} from './services/ioc/ContainerContext';
+import {store} from './redux/store';
+import {Provider} from 'react-redux';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Screen />
-    </NavigationContainer>
+    <ContainerContext.Provider value={container}>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Screen />
+        </NavigationContainer>
+      </Provider>
+    </ContainerContext.Provider>
   );
 };
 
