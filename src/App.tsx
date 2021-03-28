6ecc2c2c06d9daefb2e2a528179e6e4d325437e0
Lorenzo Imperatrice <lorenzo.imperatrice@ovalmoney.com>
2021-03-28 19:49:19 +0200

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Screen />
+    </NavigationContainer>
+  );
+};
+
+export default App;
