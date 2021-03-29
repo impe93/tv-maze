f8e8e282e946849eebee43c9d74578f93a8009e4
Lorenzo Imperatrice <lorenzo.imperatrice@ovalmoney.com>
2021-03-29 12:30:42 +0200

    Add show necessary types

---
+++ src/features/show-list/show.models.ts
@@ -0,0 +1,63 @@
+export type Show = {
+  id: number;
+  url: string;
+  name: string;
+  type: string;
+  language: string;
+  genres?: string[] | null;
+  status: string;
+  runtime: number;
+  premiered: string;
+  officialSite: string;
+  schedule: Schedule;
+  rating: Rating;
+  weight: number;
+  network: Network;
+  webChannel?: null;
+  externals: Externals;
+  image: Image;
+  summary: string;
+  updated: number;
+  _links: Links;
+};
+
+export type Schedule = {
+  time: string;
+  days?: string[] | null;
+};
+
+export type Rating = {
+  average: number;
+};
+
+export type Network = {
+  id: number;
+  name: string;
+  country: Country;
+};
+
+export type Country = {
+  name: string;
+  code: string;
+  timezone: string;
+};
+
+export type Externals = {
+  tvrage: number;
+  thetvdb: number;
+  imdb: string;
+};
+
+export type Image = {
+  medium: string;
+  original: string;
+};
+
+export type Links = {
+  self: SelfOrPreviousepisode;
+  previousepisode: SelfOrPreviousepisode;
+};
+
+export type SelfOrPreviousepisode = {
+  href: string;
+};
