107c351ffcb74df6cca95802847c71cc414a46f7
Lorenzo Imperatrice <lorenzo.imperatrice@ovalmoney.com>
2021-03-31 10:59:58 +0200

    Add the header to show list page

--- src/features/show-list/ShowList.tsx
+++ src/features/show-list/ShowList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {Image, SafeAreaView, StyleSheet, View} from 'react-native';
 import {H1} from '../../components/Typography';
+import {viewportPadding} from '../../utils/const';
 
 type Props = {};
 
@@ -8,12 +9,21 @@ export const ShowList = () => {
   return (
     <SafeAreaView>
       <View style={style.titleContainer}>
-        <H1>TV Shows</H1>
+        <Image source={require('../../assets/images/movie.png')} />
+        <H1 style={style.h1}>TV Shows</H1>
       </View>
     </SafeAreaView>
   );
 };
 
 const style = StyleSheet.create({
-  titleContainer: {},
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: viewportPadding,
+    marginTop: viewportPadding,
+  },
+  h1: {
+    marginLeft: 24,
+  },
 });
