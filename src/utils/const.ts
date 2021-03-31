9c4901d525243896ca608ee53e36a4b427ffc027
Lorenzo Imperatrice <lorenzo.imperatrice@ovalmoney.com>
2021-03-31 12:58:12 +0200

    Add hr color

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,5 +1,6 @@
 export const textColor = '#333333';
 export const subtitleColor = '#BBBBBB';
 export const inputBorderColor = '#FFC300';
+export const hrColor = '#DDDDDD';
 
 export const viewportPadding = 24;
