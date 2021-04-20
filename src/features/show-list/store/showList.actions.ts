e0ec67890595eb4ddc5700d52a92e9e85af00362
Lorenzo Imperatrice <lorenzo.imperatrice@synesthesia.it>
2021-04-20 17:24:31 +0200

    Add ShowList feature's actions

---
+++ src/features/show-list/store/showList.actions.ts
@@ -0,0 +1,25 @@
+import { ApiAction } from "../../../redux/actions";
+import { IHttpClient } from "../../../services/http/HttpClient";
+import { GET_SHOWS_ACTION, SEARCH_SHOWS_BY_NAME_ACTION } from "./showList.constants";
+
+export type SearchShowsByNameType = ApiAction<typeof SEARCH_SHOWS_BY_NAME_ACTION>
+export type GetShowsType = ApiAction<typeof GET_SHOWS_ACTION>
+
+export const searchShowsByNameAction = (httpClient: IHttpClient, name: string): SearchShowsByNameType => ({
+  type: SEARCH_SHOWS_BY_NAME_ACTION,
+  apiConfig: {
+    httpClient,
+    config: {
+      params: {
+        q: name
+      }
+    }
+  }
+});
+
+export const getShowsAction = (httpClient: IHttpClient): SearchShowsByNameType => ({
+  type: GET_SHOWS_ACTION,
+  apiConfig: {
+    httpClient,
+  }
+});
---
+++ src/features/show-list/store/showList.constants.ts
@@ -0,0 +1,7 @@
+export const GET_SHOWS_ACTION: string = 'GET_SHOWS_ACTION';
+export const SEARCH_SHOWS_BY_NAME_ACTION: string = 'SEARCH_SHOWS_BY_NAME_ACTION';
+
+export const ShowListActionList = [
+  GET_SHOWS_ACTION,
+  SEARCH_SHOWS_BY_NAME_ACTION
+]
